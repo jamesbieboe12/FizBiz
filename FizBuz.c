4bfb5df745eefaf41b27728803aad4e2616de805
jamesbieboe12 <47893229+jamesbieboe12@users.noreply.github.com>
2019-03-14 08:46:29 -0400

    Update FizBuz.c

--- FizBuz.c
+++ FizBuz.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main (void){
-  for(int i = 0; i<100; i++){
+  for(int i = 0; i<=100; i++){
    
     if(i%15==0){
-      printf("fizbuzz\n");
+      printf("fizzbuzz\n");
    }    
 
     else if(i%3==0){
-        printf("fiz\n");
+        printf("fizz\n");
     }
       
      else if(i%5==0){
